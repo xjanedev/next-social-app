83581875bc66d3990e4cf3a65518613de675b07e
subin lee <cinematicmomentz@gmail.com>
2023-09-12 22:39:45 +0900

    wip

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,16 +1,16 @@
-import { addComment } from "@/service/posts";
 import { withSessionUser } from "@/util/session";
+import { addComment } from "@/service/posts";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   return withSessionUser(async user => {
     const { id, comment } = await req.json();
 
-    if (!id || comment === null) {
+    if (!id || comment == null) {
       return new Response("Bad Request", { status: 400 });
     }
 
-    return addComment(id, user.id, comment)
+    return addComment(id, user.id, comment) //
       .then(res => NextResponse.json(res))
       .catch(error => new Response(JSON.stringify(error), { status: 500 }));
   });
